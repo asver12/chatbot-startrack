5832024308c36df8026aa3dc5a9143f746dbf75a
Laines Schmalwasser <laines@web.de>
2020-01-29 09:45:58 +0100

    added sentiment in progress

--- chatbot_startrek/ChatbotStarTrek.py
+++ chatbot_startrek/ChatbotStarTrek.py
@@ -12,6 +12,8 @@ class Chatbot:
 
     def answer(self, user_input):
         character = self.sentencehandler.find_names(user_input)
+        if character is None:
+            return
         if character is not None:
             self.dialogstatehandler.set_chatpartner(character)
         return '{}'.format(self.startrackhandler.get_random_sentence(character=self.dialogstatehandler.get_chatpartner()))
--- chatbot_startrek/StarTrackHandler.py
+++ chatbot_startrek/StarTrackHandler.py
@@ -1,17 +1,61 @@
 import random
 import os
+import json
 from chatbot_startrek import json_reader, Sentences
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+try:
+    nltk.data.find('sentiment/vader_lexicon.zip')
+except LookupError:
+    nltk.download('vader_lexicon')
 
 
 class StarTrackHandler:
-    def __init__(self, path, file):
+    def __init__(self, path, file, sentiment_file="sentiment_data.json",neg_border=0.1,positive_border= 0.1):
         """
         Dataset structure:
         Series -> episodes -> character
         """
+
+        self.sid = SentimentIntensityAnalyzer()
         self.data = json_reader.read_json(os.path.join(path, file))
         self.series = self.get_series()
         self.characters = self.get_characters()
+        self.sentiment_data = []
+        self.sentiment = self.load_sentiment_data(path, sentiment_file, neg_border, positive_border)
+
+    def load_sentiment_data(self, path, file, neg_border=0.1, positive_border=0.1):
+        if os.path.isdir(path):
+            file_path = os.path.join(path, file)
+            if os.path.isfile(file_path):
+                print("Loading sentiment data")
+                with open(file_path) as target:
+                    return json.load(target)
+            else:
+                sentiment = {"neg": [], "neu": [], "pos": []}
+                for series in self.data.values():
+                    for episode in series.values():
+                        for character in episode.values():
+                            for sentence in character:
+                                if len(sentence) != 0:
+                                    sentiment[self.get_sentiment(sentence, neg_border, positive_border)].append(sentence)
+
+                with open(file_path, "w") as target:
+                    json.dump(sentiment, target)
+                return sentiment
+        else:
+            raise FileNotFoundError("Path: {} does not exist".format(path))
+
+    def get_sentiment(self, sentence, neg_border=0.1, positive_border=0.1):
+        if self.sid.polarity_scores(sentence)["neg"] > neg_border:
+            return "neg"
+        elif self.sid.polarity_scores(sentence)["pos"] > positive_border:
+            return "pos"
+        else:
+            return "neu"
+
+    def get_sentiment_sentence(self, user_input, sentiment, neg_border=0.1, positive_border=0.1):
+        return random.choice(self.sentiment[self.get_sentiment(user_input, neg_border, positive_border)])
 
     def get_random_sentence(self, series=None, episode=None, character=None, iter=10000):
         sentences = []
